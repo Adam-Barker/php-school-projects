7e0a1e84f643f3294f7451815f47440957808ff8
Adam-Barker <adam.barker311@gmail.com>
2015-10-22 18:41:58 -0400

    Added Programs

---
+++ Program_3/bobscars.php
@@ -0,0 +1,93 @@
+<?php
+$data =
+"Dodge Avenger
+2006
+DA111-9
+4
+35.5
+107
+21297
+.2
+Monthly
+Olds Alero
+2004
+OA340-1
+5
+29.95
+137
+23335
+.2
+Monthly
+Chrysler PT Cruiser
+2003
+CPTC-MW2
+4
+37.95
+31
+15405
+.2
+Weekly
+Cadillac Limo
+1999
+1999-01
+18
+142.5
+3
+38900
+.1
+Weekly
+Chevrolet Monte Carlo
+1997
+CMC-21
+6
+27.3
+68
+19437
+.2
+Monthly
+Chevrolet Suburban
+1997
+CSB-011
+8
+42.75
+13
+29999
+.2
+Monthly
+VW Bus T2A
+1977
+VWB-09
+32
+15
+22
+12000
+NA
+Daily
+Ford Station Wagon
+1976
+FSW-67
+8
+10.95
+12
+9899
+NA
+Whenever
+Toyota Forklift
+1997
+6FGCU-45
+1
+61.25
+73
+8795
+.1
+Yearly
+Caterpillar Dozer (D7H)
+1989
+1989-11
+1
+98
+4
+67850
+.1
+Yearly";
+?>
